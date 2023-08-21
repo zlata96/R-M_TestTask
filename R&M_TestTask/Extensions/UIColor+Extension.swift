b794de551262bf47241eac46ea78175e89093ceb
Zlata Guseva <gusevazlata96@gmail.com>
2023-08-21 16:42:00 +0400

    Add Fonts and Colors

---
+++ R&M_TestTask/Extensions/UIColor+Extension.swift
@@ -0,0 +1,10 @@
+// UIColor+Extension.swift
+// R&M_TestTask. Created by Zlata Guseva.
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let grayBackground = UIColor(named: "grayBackground")
+    static let mainBackground = UIColor(named: "mainBackground")
+}
---
+++ R&M_TestTask/Extensions/UIFont+Extension.swift
@@ -0,0 +1,14 @@
+// UIFont+Extension.swift
+// R&M_TestTask. Created by Zlata Guseva.
+
+import UIKit
+
+extension UIFont {
+    static var text1SB: UIFont {
+        UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.semibold)
+    }
+
+    static var text1B: UIFont {
+        UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.bold)
+    }
+}
