c0c82c2bd0872c49d639a0cdd5eec4a8e385015c
Zlata Guseva <gusevazlata96@gmail.com>
2023-08-21 13:34:15 +0400

    Initial Commit

---
+++ R&M_TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R&M_TestTask
+//
+//  Created by Злата Гусева on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
