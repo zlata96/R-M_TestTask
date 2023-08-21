d04ed514949390e4d35760d765996fc541ab6040
Zlata Guseva <gusevazlata96@gmail.com>
2023-08-22 00:03:52 +0400

    Add new assets

--- R&M_TestTask/Extensions/UIColor+Extension.swift
+++ R&M_TestTask/Extensions/UIColor+Extension.swift
@@ -7,4 +7,8 @@ import UIKit
 extension UIColor {
     static let grayBackground = UIColor(named: "grayBackground")
     static let mainBackground = UIColor(named: "mainBackground")
+    static let subtitle = UIColor(named: "subtitle")
+    static let accentTitle = UIColor(named: "accentTitle")
+    static let littleTitle = UIColor(named: "littleTitle")
+    static let imageBackgrounnd = UIColor(named: "imageBackgrounnd")
 }
--- R&M_TestTask/Extensions/UIFont+Extension.swift
+++ R&M_TestTask/Extensions/UIFont+Extension.swift
@@ -11,4 +11,20 @@ extension UIFont {
     static var text1B: UIFont {
         UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.bold)
     }
+
+    static var text1M: UIFont {
+        UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+    }
+
+    static var text2M: UIFont {
+        UIFont.systemFont(ofSize: 13, weight: UIFont.Weight.medium)
+    }
+
+    static var text3M: UIFont {
+        UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.medium)
+    }
+
+    static var text2B: UIFont {
+        UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
+    }
 }
