029a5a7b6bd5a8a1f12ef6447678bcea8dc2576d
Zlata Guseva <gusevazlata96@gmail.com>
2023-08-21 14:13:43 +0400

    Initial commit

--- R&M_TestTask/AppDelegate.swift
+++ R&M_TestTask/AppDelegate.swift
@@ -1,36 +1,25 @@
-//
-//  AppDelegate.swift
-//  R&M_TestTask
-//
-//  Created by Злата Гусева on 21.08.2023.
-//
+// AppDelegate.swift
+// R&M_TestTask. Created by Zlata Guseva.
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        setupRootViewController()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    private func setupRootViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        let navigationVC = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationVC
+        window?.makeKeyAndVisible()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
-
--- R&M_TestTask/ViewController.swift
+++ R&M_TestTask/ViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
-//  R&M_TestTask
-//
-//  Created by Злата Гусева on 21.08.2023.
-//
+// ViewController.swift
+// R&M_TestTask. Created by Zlata Guseva.
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
-
-
 }
-
